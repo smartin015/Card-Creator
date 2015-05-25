3ffabfc35b0b1bf3fd2ab86177d745f34dd9dda0
Todd Medema <toddmedema@gmail.com>
2015-05-25 09:41:59 -0400

    new icons, arbitrary filters

--- js/generate.js
+++ js/generate.js
@@ -1,3 +1,23 @@
+var filters, filterList, filterCount;
+function fetchFilters() {
+  var match,
+      pl     = /\+/g,  // Regex for replacing addition symbol with a space
+      search = /([^&=]+)=?([^&]*)/g,
+      decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+      query  = window.location.search.substring(1);
+
+  filters = {};
+  filterList = [];
+  while (match = search.exec(query)) {
+    var f = decode(match[1]);
+    filters[f] = decode(match[2]);
+    filterList.push(f);
+  }
+  filterCount = filterList.length;
+  console.log(filters)
+}
+
+
 Swag.registerHelpers(); // lots of handlebars helpers: https://github.com/elving/swag
 // http://blog.stevenlevithan.com/archives/javascript-roman-numeral-converter
 Handlebars.registerHelper("romanize", function(num) {
@@ -11,7 +31,6 @@ Handlebars.registerHelper("romanize", function(num) {
   while (i--) roman = (key[+digits.pop() + (i * 10)] || "") + roman;
   return Array(+digits.join("") + 1).join("M") + roman;
 });
-
 var templates = { // will be rendered into UI in this order
   Character: Handlebars.compile($("#character-template").html()),
   Encounter: Handlebars.compile($("#encounter-template").html()),
@@ -42,7 +61,7 @@ $(function() {
     }, simpleSheet: true
   });
   $("#showAll").click(function() {
-    history.replaceState({}, document.title, $.query.set('filter', '').toString());
+    history.replaceState({}, document.title, '?');
     render();
   });
 });
@@ -50,15 +69,13 @@ $(function() {
 function render() {
   $(".page").remove(); // clear out any cards from past renders
   cardCount = 0;
+  fetchFilters();
 
-  var filter = getURLParam('filter'),
-      sheets = tabletop.sheets(),
+  var sheets = tabletop.sheets(),
       sorted = [];
 
-  for (var key in templates) { // if a filter is applied, limit what we show
-    if (!filter || key === filter) {
-      sorted[sorted.length] = key;
-    }
+  for (var key in templates) {
+    sorted[sorted.length] = key;
   }
   for (var i = 0, l = sorted.length; i < l; i++) { // sort by type in order listed in var templates
     sorted[i] = sheets[sorted[i]];
@@ -71,10 +88,17 @@ function render() {
 
 function makeCards(template, cards) {
   for (var i = 0, l = cards.length; i < l; i++) {
-    var card = cards[i];
+    var card = cards[i], filteredOut = false;
+    card.cardType = template;
+    console.log(card);
 
     // define filters / skips here
     if (card.Comment !== "") { continue; }
+    for (var j = 0; j < filterCount; j++) {
+      console.log(template, filterList[j], card[filterList[j]], filters[filterList[j]]);
+      if (card[filterList[j]] !== filters[filterList[j]]) { filteredOut = true; continue; }
+    }
+    if (filteredOut) { continue; }
 
     if (cardCount % 6 === 0) { // new page every 6
       fronts = $('<div class="page fronts"></div>');
@@ -85,7 +109,6 @@ function makeCards(template, cards) {
     for (var property in card) {
       if (card[property] === '-') { card[property] = ''; }
     }
-    card.cardType = template;
     fronts.append(templates[template](card));
     backs.append(backTemplate(card));
     cardCount++;
@@ -94,15 +117,9 @@ function makeCards(template, cards) {
 
 function makeToggleButton(title) {
   var b = $("<button>Only " + title + "</button>");
-  b.attr('id', 'filter' + title);
   b.click(function(e) {
-    history.replaceState({}, document.title, $.query.set('filter', title).toString());
+    history.replaceState({}, document.title, $.query.set('cardType', title).toString());
     render();
   });
   $("#toggleButtons").append(b);
-}
-
-function getURLParam(name) {
-  var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href) || 0;
-  return results[1] || 0;
 }
