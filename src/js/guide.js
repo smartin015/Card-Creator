88fc8a2b8df77717adf3c775370a3125f16c5ab5
Scott Martin <smartin015@gmail.com>
2015-11-01 19:34:57 -0500

    Halfway through guide guide remake

--- src/js/guide.js
+++ src/js/guide.js
@@ -2,7 +2,7 @@
   createView();
 
   Tabletop.init({
-    key: '1dizpALvuAVxe4J_9NGk14GzAqlwapc94w3HgUD0rLWo',
+    key: '1WvRrQUBRSZS6teOcbnCjAqDr-ubUNIxgiVwWGDcsZYM',
     callback: function(d, t) {
       console.log('sheets loaded');
 
@@ -35,12 +35,12 @@ function createView() {
   content.append(iconTextTmpl({
     id: "intro",
     title: "Expedition is Different",
-    img: "img/d20.svg",
-    p1: "We built it from the ground up to be thoughtful and challenging, but not slow and overwhelming. "
-      + "This especially applies to the typical \"dungeon master\" role seen in most roleplaying games.",
+    img: "img/icon/roll.svg",
+    p1: "This game is built from the ground up to be thoughtful and challenging, but not slow and overwhelming. "
+      + "Even the typical \"game master\" role seen in most roleplaying games becomes far easier and more fluid.",
     list: [
-      "There's less number-crunching, but every decision is just as meaningful and open-ended. You'll have more time to focus on telling the story.",
-      "Everything is card-based; for long campaigns, players can (and should) take their decks home and personalize them between games.",
+      "There's less number-crunching, but every decision is just as open-ended. There's more time to focus on telling the story.",
+      "Everything is card-based. For long campaigns, players can (and should) take their decks home and personalize them between games.",
       "Enemy encounters resolve within minutes - you'll be kept on your toes by your party's actions.",
       "Made-up rules (\"House Rules\") are encouraged, and add significant depth to the game.",
     ],
@@ -49,83 +49,41 @@ function createView() {
   content.append(iconTextTmpl({
     id: "requirements",
     title: "Requirements",
-    img: "img/int.svg",
-    p1: "A standard campaign requires one Guide and three to five Explorers, plus:",
+    img: "img/icon/adventurer.svg",
+    p1: "A standard game requires one Guide and three to five Adventurers, plus:",
     list: [
       "A copy of the cards, either purchased or downloaded and printed.",
       "A phone or tablet running the Expedition App.",
       "A 20-sided die for every player.",
-      "A large whiteboard surface, with enough markers for everyone. In a pinch, you can also you pencils and paper (one pencil and sheet of paper for each person, plus one sheet for the center)."
+      "Some pencil, paper, and paperclips."
     ],
   }));
   
   content.append(headerTmpl({id: "setup", title: "Setting Up"}));
   
   content.append(iconTextTmpl({
-    id: "choose-decks",
-    title: "1. Choose your Decks",
-    img: "img/title.svg",
-    p1: "From your main set and expansions (you have the expansions, right?) select:",
-    list: [
-      "<strong>6 Encounter</strong> decks, including <strong>1 Trap</strong> deck",
-      "<strong>4 Ability</strong> decks",
-      "<strong>50-80 Loot</strong> cards.",
-      "<strong>1 Explorer</strong> deck.",
-      "<strong>1 Title</strong> deck.",
-    ],
-    p2: "If you don't have any expansions, the main set contains exactly the cards you need.",
-  }));
-  
-  content.append(iconTextTmpl({
     id: "choose-abilities",
-    title: "2. Choose Explorers and Abilities",
-    img: "img/class/Ranged.svg",
-    p1: "Your party should now choose their Explorer and Abilities:",
+    title: "2. Choose Adventurers and Abilities",
+    img: "img/icon/Ranger.svg",
+    p1: "Your party starts by choosing their Adventurer and Abilities:",
     list: [
-      "Pass the Explorer deck around, allowing everyone to shuffle through and choose their character.",
+      "Pass the Adventurer deck around, allowing everyone to shuffle through and choose their character.",
       "Make sure everyone names their character. Suggest brainstorming a matching backstory, as well.",
       "Direct each player to choose their starting abilities from one of the ability decks.",
     ],
     p2: "Further instructions on choosing explorers and abilities can be found in the "
-      + "<a href=\"/handbook.html#setup\" target=\"_blank\">Setting Up</a> section of the Explorer's Handbook.",
+      + "<a href=\"/handbook.html#setup\" target=\"_blank\">Setting Up</a> section of the Adventurer's Handbook.",
   }));    
-  
-  content.append(iconTextTmpl({
-    id: "choose-equipment",
-    title: "3. Gear Up",
-    img: "img/loot.svg",
-    p1: "Your explorers need equipment!",
-    list: [
-      "Pick at least twice the number of Loot from the deck as there are players.",
-      "Include one or more weapons for each of the starting ability decks.",
-      "Healing loot and other cheap items are also good choices.",
-      "Place the cards face up in a grid. Players can take 25 gold worth of items.",
-    ],
-  }));    
-  
-  content.append(headerTmpl({id: "trouble", title: "Inventing Trouble"}));
-  
+
   content.append(iconTextTmpl({
     id: "trouble-encounter",
     title: "Plan your Encounters",
-    img: "img/encounter.svg",
+    img: "img/icon/Fae.svg",
     p1: "Start thinking about your next encounter early on.",
     list: [
       "It should be as hard or just a bit tougher than your party can currently handle.",
       "Remember your party's health drops as they fight, so weigh the encounter accordingly.",
       "Try combining Encounters (\"spider-dogs\", etc) to combine Health, Tier, and special abilities.",
-    ],
-  }));    
-  
-  content.append(iconTextTmpl({
-    id: "trouble-mayhem",
-    title: "Cause Mayhem",
-    img: "img/trap.svg",
-    p1: "Look for an excuse to cause mayhem.",
-    list: [
-      "If you see players getting comfortable with a certain tactic, figure out how to break it!",  
-      "You can Ambush the party with an encounter and require them to shuffle their ability decks to disrupt their plans",
-      "Make sure there's a good reason in the story for these disruptions to happen, or your party may feel frustrated.", 
     ],
   }));    
   
@@ -146,26 +104,16 @@ function createView() {
   content.append(iconTextTmpl({
     id: "encounters-rounds",
     title: "Encounter Rounds",
-    img: "img/dex-1.svg",
+    img: "img/icon/damage.svg",
     p1: "Once you're ready to start the round, notify everyone that battle has begun!",
     list: [
       "Tap the app to start the timer.", 
       "Players should draw their three Ability cards and play one, quickly.",
-      "Once every player has played, stop the timer by tapping it",
+      "Once every player has played, stop the timer by tapping it.",
       "Resolve the round, including the damage listed on the app.",
+      "When a surge of a given Threat occurs, resolve the actions listed under the Surge section of all the Encounter cards at that Threat level."
     ],
   }));    
-  
-  content.append(iconTextTmpl({
-    id: "encounters-surges",
-    title: "Encounter Surges",
-    img: "img/class/Defense.svg",
-    p1: "Surge events also occur during battle.",
-    list: [  
-      "When a surge of a given Threat occurs, choose an Encounter with that Threat level and resolve the actions listed under the Surge section.",
-      "If no Encounters exist at that level, choose first from higher Threat, and then from low Threat if there are no Encounters with high Threat.",
-    ],
-  }));  
   
   content.append(iconTextTmpl({
     id: "encounters-storytelling",
@@ -237,9 +185,7 @@ function createView() {
       "Burning buildings can create falling wreckage; underground exploration risks a cave-in.",
     ],
   }));    
-  
-  content.append(headerTmpl({id: "npcs", title: "Merchants and Inns"}));
-  
+    
   content.append(iconTextTmpl({
     id: "npcs-merchants",
     title: "Merchants",
